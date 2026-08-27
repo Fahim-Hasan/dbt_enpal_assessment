a61ca84302cc07cc2b8752fcaa1737ffabb1a7ef
Fahim-Hasan <fahimhasan72@gmail.com>
2026-08-27 00:11:31 +0000

    Date spine fix

--- models/intermediate/dim_dates/int_pipedrive__date_spine.sql
+++ models/intermediate/dim_dates/int_pipedrive__date_spine.sql
@@ -1,11 +1,22 @@
-{% set unioned_event_dates %}
+with unioned_event_dates as (
     select changed_at as event_date from {{ ref('stg_pipedrive__deal_changes') }}
     union all
     select due_at as event_date from {{ ref('stg_pipedrive__activity') }}
-{% endset %}
+),
 
-{{ dbt_utils.date_spine(
-    datepart="day",
-    start_date="(select min(event_date)::date from (" ~ unioned_event_dates ~ ") as bounds)",
-    end_date="(select max(event_date)::date + interval '1 day' from (" ~ unioned_event_dates ~ ") as bounds)"
-) }}
+bounds as (
+    select
+        min(event_date)::date as min_date,
+        max(event_date)::date as max_date
+    from unioned_event_dates
+),
+
+date_spine as (
+    -- Native Postgres generate_series, evaluated by the database at query time -- avoids
+    -- dbt_utils.date_spine's dynamic-subquery-bounds pattern, which requires run_query() at
+    -- compile time and returns None
+    select generate_series(min_date, max_date, interval '1 day')::date as date_day
+    from bounds
+)
+
+select date_day from date_spine
